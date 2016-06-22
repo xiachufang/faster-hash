61c3defb1f1ce6bda8d9b8c9701b664e721c6e8a
Alex.F <gfreezy@gmail.com>
2016-06-22 12:24:12 +0800

    bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 from distutils.core import setup
-from Cython.Build import cythonize
+from distutils.extension import Extension
+
+USE_CYTHON = False   # command line option, try-import, ...
+
+ext = '.pyx' if USE_CYTHON else '.c'
+
+extensions = [Extension("faster_hash", ["faster_hash"+ext])]
+
+if USE_CYTHON:
+    from Cython.Build import cythonize
+    extensions = cythonize(extensions)
 
 setup(
     name='faster_hash',
-    version='0.0.1',
+    version='0.0.2',
+    url='https://github.com/xiachufang/faster-hash',
     description='fnv1a in Cython',
     author='gfreezy',
     author_email='gfreezy@gmail.com',
-    install_requires=['Cython'],
-    ext_modules=cythonize("faster_hash.pyx")
+    ext_modules=extensions
 )
