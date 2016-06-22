23c74d53639194fec5a6d8af6ad7de7e99375941
Alex.F <gfreezy@gmail.com>
2016-06-22 12:28:03 +0800

    use setuptools instead of distutils

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup
+from setuptools.extension import Extension
 
 USE_CYTHON = False   # command line option, try-import, ...
 
