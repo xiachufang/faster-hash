748307e53f192f88b43b1827b484afc1a48a61f9
John Hu <hszqqq12@gmail.com>
2019-02-01 16:20:07 +0800

    Add doctest

--- faster_hash.pyx
+++ faster_hash.pyx
@@ -1,6 +1,17 @@
 # cython: language_level=2
 
 cpdef long fnv1a(char* s):
+    """Fowler–Noll–Vo hash function, see: https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
+
+    >>> fnv1a(b'3ce6330f-ab86-45df-b2ff-b16cad4f24f1')
+    872280216
+    >>> fnv1a(b'31eef3c2-42b8-4959-abd4-dc559aba7331')
+    2859758067
+    >>> fnv1a(b'646f39fb-c3bb-440b-befc-9aa56559d131')
+    3424154349
+    >>> fnv1a(b'3dc0be7a-30ca-452c-8cf0-19364f697a14')
+    4099963837
+    """
     cdef long prime = 0x01000193
     cdef long h = 0x811c9dc5
     cdef long f = 0xffffffff
