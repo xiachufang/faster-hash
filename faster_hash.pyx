ca63fc9a736b437b7c86ce7f3001539307d129f2
John Hu <hszqqq12@gmail.com>
2019-02-01 14:32:32 +0800

    Declare 0xffffffff as long in C

--- faster_hash.pyx
+++ faster_hash.pyx
@@ -3,7 +3,8 @@
 cpdef long fnv1a(char* s):
     cdef long prime = 0x01000193
     cdef long h = 0x811c9dc5
+    cdef long f = 0xffffffff
     cdef char c
     for c in s:
-        h = (h ^ c * prime) & 0xffffffff
+        h = (h ^ c * prime) & f
     return h
